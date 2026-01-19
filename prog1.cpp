c9a23171c2a31abb486795812beb66cc784a8141
Ishnika <ishnika25csu091@ncuindia.edu>
2026-01-19 20:49:09 +0530

    prog1.cpp
    print character string integer and float

---
+++ prog1.cpp
@@ -0,0 +1,12 @@
+// print character string integer anf float 
+#include<iostream>
+int main()
+{
+    int a=10;
+    float b=10.5;
+    char c='A';
+    std::string name="ishnika";
+    std::cout<<"the value of a="<<a<<std::endl<<"the value of f="<<b<<std::endl<<"the value of c="<<c<<std::endl<<"the string="
+    <<name;
+    return 0;
+}
